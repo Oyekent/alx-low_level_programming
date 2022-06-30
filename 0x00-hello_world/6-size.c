86821313323be598401098eb0062559eed0407b1
Oyemike Chukuneku <oyemikehc@gmail.com>
2022-06-30 16:45:55 -0700

    Task 6 commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - A C program that prints the size of various types on the computer it is compiled and run on.
+* 
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char c;
+int i;
+long int li;
+long long int lli;
+float f;
+printf("Size of a char: %c byte(s)\n", sizeof(c));
+printf("Size of an int: %i byte(s)\n", sizeof(i));
+printf("Size of a long int: %li byte(s)\n", sizeof(li));
+printf("Size of a long long int: %lli byte(s)\n", sizeof(lli));
+printf("Size of a float: %f byte(s)\n", sizeof(f));
+return (0);
+}
