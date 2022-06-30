2052deb586fb30720638dc8d2b23cf2317a3faf3
Oyemike Chukuneku <oyemikehc@gmail.com>
2022-06-30 13:43:51 -0700

    Task 4 commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
@@ -10,5 +10,5 @@ int main(void)
 {
 	Puts("\"Programming is like building a multilingual puzzle\n");
 
-	Return 0;
+	Return (0);
 }
