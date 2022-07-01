7962b5afb73b7e56b9bb7e321e5527339b5f5863
Oyemike Chukuneku <oyemikehc@gmail.com>
2022-07-01 09:56:55 -0700

    Task 3 commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Prints the alphabet in lowercase and then in uppercase with a new line
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+for (letter = 'A'; letter <= 'Z'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
+}
