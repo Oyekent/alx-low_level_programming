689f03b683fb988fe1a70be84723b0b416b05886
Oyemike Chukuneku <oyemikehc@gmail.com>
2022-06-30 17:17:40 -0700

    Task 6 commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - A C program that prints the size of various types on the computer it is compiled and run on.
+* main - Entry point
 *
 * Return: Always 0 (Success)
 */
@@ -12,10 +12,10 @@ int i;
 long int li;
 long long int lli;
 float f;
-printf("Size of an char: %i byte(s)\n", sizeof(c));
-printf("Size of an int: %i byte(s)\n", sizeof(i));
-printf("Size of a long int: %li byte(s)\n", sizeof(li));
-printf("Size of a long long int: %lli byte(s)\n", sizeof(lli));
-printf("Size of a float: %f byte(s)\n", sizeof(f));
+printf("Size of an char: %lu byte(s)\n", sizeof(c));
+printf("Size of an int: %lu byte(s)\n", sizeof(i));
+printf("Size of a long int: %lu byte(s)\n", sizeof(li));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(lli));
+printf("Size of a float: %lu byte(s)\n", sizeof(f));
 return (0);
 }
