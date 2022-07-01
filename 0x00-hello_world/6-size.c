a1ee304f01bf9d3df65824fba647d5aea902642b
Oyemike Chukuneku <oyemikehc@gmail.com>
2022-06-30 17:26:07 -0700

    Task 6 commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,12 +10,12 @@ int main(void)
 char c;
 int i;
 long int li;
-long long int ll;
-float f;
+long long int lli;
+float d;
 printf("Size of an char: %lu byte(s)\n", sizeof(c));
 printf("Size of an int: %lu byte(s)\n", sizeof(i));
 printf("Size of a long int: %lu byte(s)\n", sizeof(li));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
-printf("Size of a float: %lu byte(s)\n", sizeof(f));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(lli));
+printf("Size of a float: %lu byte(s)\n", sizeof(d));
 return (0);
 }
