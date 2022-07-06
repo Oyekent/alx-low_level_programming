1fd2cb3abc8b5f4e7c9b34fd8692c3b854bac7ab
Oyemike Chukuneku <oyemikehc@gmail.com>
2022-07-05 18:13:18 -0700

    Task 0 and main.h commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* main - This program prints _putchar, followed by a new line.
+*
+* Return - 0.
+*
+*/
+
+void _putchar(void);
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchat('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	-putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+A README.md file for functions, nested loops tasks
