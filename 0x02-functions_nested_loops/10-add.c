9037d2c4bedd11a3c8af11df0a85320f424397f2
Oyemike Chukuneku <oyemikehc@gmail.com>
2022-07-06 12:55:51 -0700

    Task 10 commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* add - Adds two integers and returns the result.
+*
+* @num1: Argument 1
+* @num2: Argument 2.
+*
+* Return: Result of the sum.
+*/
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
