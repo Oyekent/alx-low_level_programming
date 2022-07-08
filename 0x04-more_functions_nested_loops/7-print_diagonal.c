61ca8267acf9828d59b6ccfd48a917266cd78b10
Oyemike Chukuneku <oyemikehc@gmail.com>
2022-07-08 14:16:42 -0700

    Task 7 commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,23 +7,18 @@
 */
 void print_diagonal(int n)
 {
-	int line, space;
-
-	if (n > 0)
-	{
-		for (line = 0; line < n; line++)
-		{
-		for (space = 0; space <= line; space++)
-			_putchar(' ');
-
-			_putchar('\\');
-
-			if (line == n - 1)
-
-				continue;
-
-			_putchar('\n');
-		}
-	}
-	_putchar('\n');
+int line, space;
+if (n > 0)
+{
+for (line = 0; line < n; line++)
+{
+for (space = 0; space <= line; space++)
+_putchar(' ');
+_putchar('\\');
+if (line == n - 1)
+continue;
+_putchar('\n');
+}
+}
+_putchar('\n');
 }
