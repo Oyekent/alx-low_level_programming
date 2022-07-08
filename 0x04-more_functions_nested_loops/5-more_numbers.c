962e1aca5a9d184e55608136aa247be90e2a3cbb
Oyemike Chukuneku <oyemikehc@gmail.com>
2022-07-08 13:21:24 -0700

    Task commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,14 +2,13 @@
 
 /*
 * more_numbers - Prints the numbers 0-14 ten times followed by a new line.
-*
 * Return 0
 *
 */
 void more_numbers(void)
 {
 	int num, count;
-	
+
 	for (count = 0; count <= 9; count++)
 	{
 	for (num = 0; num <= 14; num++)
