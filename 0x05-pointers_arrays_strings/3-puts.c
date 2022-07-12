396c433f56b5aaa15f72447068fff81347f2eb99
Oyemike Chukuneku <oyemikehc@gmail.com>
2022-07-12 12:09:54 -0700

    Task 3 commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - A function that prints a string, followed by a new line to stdout.
+ *
+ * @str: String to be printed.
+ */
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+
+	_putchar('\n');
+}
