b86e4861c7e6aab56abc4b7ad3217d520d5517f9
Oyemike Chukuneku <oyemikehc@gmail.com>
2022-07-12 12:36:18 -0700

    Task 4 commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,13 +7,16 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int index = 0;
 
-	while (s[index++])
-		len++;
-
-	for (index = len - 1; index >= 0; index--)
+	while (s[index] != 0)
+	{
+		index++;
+	}
+	while (index > 0)
+	{
+		index--;
 		_putchar(s[index]);
-
+	}
 	_putchar('\n');
 }
