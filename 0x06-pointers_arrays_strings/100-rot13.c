8c2ace2ec703cd98b6cfcbd5c4a72703425953dd
Oyemike Chukuneku <oyemikehc@gmail.com>
2022-07-14 15:01:23 -0700

    Task 100 commit

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rot13 - A function that encodes a string into rot13.
+ *
+ * @s: The string to encode.
+ *
+ * Return: Result.
+ */
+char *rot13(char *s)
+{
+	int a = 0;
+
+	while (s[a])
+	{
+		while ((s[a] >= 'a' && s[a] <= 'z') || (s[a] >= 'A' && s[a] <= 'Z'))
+		{
+			if ((s[a] > 'm' && s[a] <= 'z') || (s[a] > 'M' && s[a] <= 'Z'))
+			{
+				s[a] -= 13;
+				break;
+			}
+
+			s[a] += 13;
+			break;
+		}
+
+		a++;
+	}
+
+	return (s);
+}
