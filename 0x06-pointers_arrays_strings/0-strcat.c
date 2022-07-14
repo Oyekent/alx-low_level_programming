bfbd7814708cfd68b4f2a69fe7a6595f7a5bfa77
Oyemike Chukuneku <oyemikehc@gmail.com>
2022-07-14 12:55:56 -0700

    Task 1 commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * _strcat - A function that concatenates two strings.
+  *
+  * @dest: Destination string.
+  * @src: Source string.
+  *
+  * Return: A pointer to the resulting string dest.
+  */
+ char *_strcat(char *dest, char *src)
+ {
+ 	char *s = dest;
+
+ 	while (*dest != '\0')
+ 	{
+ 		dest++;
+ 	}
+
+ 	while (*src != '\0')
+ 	{
+ 		*dest = *src;
+ 		dest++;
+ 		src++;
+ 	}
+ 	*dest = '\0';
+ 	return (s);
+ }
