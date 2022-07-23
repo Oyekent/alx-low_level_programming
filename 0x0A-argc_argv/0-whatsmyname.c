04080b7597a718f1bffacffdf238592c902a43d5
Oyemike Chukuneku <oyemikehc@gmail.com>
2022-07-22 19:46:50 -0700

    Task 0 commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - A program that prints its name, followed by a new line.
+ *
+ * @argc: The number of command line arguments or the size of @argv array.
+ * @argv: The array containing the program command line arguments
+ * or the array of size @argc.
+ *
+ * Return: 0.
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
