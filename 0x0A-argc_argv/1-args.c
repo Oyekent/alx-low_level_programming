426103dbab0538de8bab890bcfe96a770fa02bc3
Oyemike Chukuneku <oyemikehc@gmail.com>
2022-07-22 20:31:45 -0700

    Task 1 commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - A program that prints the number of arguments passed into it.
+ *
+ * @argc: The number of command line arguments or the size of @argv array.
+ * @argv: The array containing the program command line arguments
+ * or the array of size @argc.
+ *
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
