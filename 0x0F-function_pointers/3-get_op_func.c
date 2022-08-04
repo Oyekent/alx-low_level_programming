37123ab8a954a976a454c1c772735e41202c133a
Oyemike Chukuneku <oyemikehc@gmail.com>
2022-08-03 18:07:53 -0700

    Task 3 commit

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * get_op_func - A function that selects the correct function to perform
@@ -22,8 +23,13 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
-		i++;
+	while (i < 5)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
 
-	return (ops[i].f);
+		i++;
+	}
+
+	return (0);
 }
