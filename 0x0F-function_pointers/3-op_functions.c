c4f4d4ef5ce611e700782662ebc0b020dea02d72
Oyemike Chukuneku <oyemikehc@gmail.com>
2022-08-03 18:29:05 -0700

    Task 3 commit

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -56,6 +56,7 @@ int op_div(int a, int b)
 		printf("Error\n");
 		exit(100);
 	}
+
 	return (a / b);
 }
 
@@ -69,10 +70,11 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
-	if b(== 0)
+	if b( == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
+
 	return (a % b);
 }
