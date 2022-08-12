30dbab231f6c806b2131fd763e3d4d8048f55d07
Oyemike Chukuneku <oyemikehc@gmail.com>
2022-08-11 17:15:44 -0700

    Task 100 commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/* myFunction prototype */
+void myFunction(void);
+
+/**
+ * myFunction - Prints a string before the main function is executed.
+ */
+void myFunction(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
+}
