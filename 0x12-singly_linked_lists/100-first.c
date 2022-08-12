8d2f49020b9e0b37e107dbe94c94256373aa2eb8
Oyemike Chukuneku <oyemikehc@gmail.com>
2022-08-11 17:48:53 -0700

    Task 100 commit

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,14 @@
 #include "lists.h"
 
+/*
+ * Apply constructor attribute to myFunction() so that,
+ * it is executed before main().
+ */
+void myFunction(void) __attribute__((constructor));
+
 /**
  * myFunction - Prints a string before the main function is executed.
  */
-void myFunction(void);
-
 void myFunction(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
